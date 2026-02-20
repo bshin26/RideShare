956eabb95b5dc5612e541c5c00a648d17af61fd6
Brandon Shin <brandonshin26@gmail.com>
2026-02-20 12:22:50 -0800

    Fields, Constructors, and methods for Passanger.java

--- Passanger.java
+++ Passanger.java
@@ -1,3 +1,35 @@
 public class Passanger {
+    // Fields
+    private int destination;
+    private boolean direction; // Will return true if going right and false if going left
+    private int start;
 
+    // Constructors
+    public Passanger(int myDestination, int myStart){
+        destination = myDestination;
+        start = myStart;
+
+        if(destination > start){
+            direction = true;
+        } else {
+            direction = false;
+        }
+    }
+
+    // Methods
+    public int getDestination(){
+        return destination;
+    }
+
+    public boolean getDirection(){
+        return direction;
+    }
+
+    public int getStart(){
+        return start;
+    }
+
+    public String toString(){
+        return super.toString() + " Start: " + start + " Destination: " + destination;
+    }
 }
