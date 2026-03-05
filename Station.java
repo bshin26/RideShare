e5d095e34a25e722551b7b9dfff2d6f04f47636d
Brandon Shin <brandonshin26@gmail.com>
2026-03-04 23:42:02 -0800

    started station.java

--- Station.java
+++ Station.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class Station {
+    // Fields
+    private int stationNumber;
+    private ArrayList<Passanger> waitingLeft;
+    private ArrayList<Passanger> waitingRight;
+    private ArrayList<Passanger> done;
     
+    // Constructors
+    public Station(int myStationNumber){
+        stationNumber = myStationNumber;
+        waitingLeft = new ArrayList<Passanger>();
+        waitingRight = new ArrayList<Passanger>();
+        done = new ArrayList<Passanger>();
+    }
+
+    // Methods
+
 }
